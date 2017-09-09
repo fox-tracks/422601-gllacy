df5d6ea3d3c29382c6030881f3d089de257d19ff
fox-tracks <t.andriyanova@yahoo.com>
2017-09-09 23:47:14 +0300

    доделала метку на карте

--- js/common.js
+++ js/common.js
@@ -4,14 +4,15 @@ ymaps.ready(init);
 function init (){
     myMap = new ymaps.Map('map', {
         center: [59.939536, 30.329119],
-        zoom: 16
+        zoom: 16,
+        controls: []
     });
     myPlacemark = new ymaps.Placemark ([59.938631, 30.323055], {
         hintContent: 'Мы здесь!'
     }, {
         iconLayout: 'default#image',
-        iconImageHref: 'img/pin.svg',
-        iconImageSize: [80, 140],
+        iconImageHref: 'img/pin-and-shadow.png',
+        iconImageSize: [218, 142],
         iconImageOffset: [-38, -140]
     });
     myMap.geoObjects
