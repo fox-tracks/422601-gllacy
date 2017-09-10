9858c6fff9faa0cbb3d0ef504b11a80044215ce6
fox-tracks <t.andriyanova@yahoo.com>
2017-09-11 00:30:17 +0300

    пределала паддинги у центровщика на марджины у вложенных элементов, начала js.

--- js/common.js
+++ js/common.js
@@ -18,3 +18,40 @@ function init (){
     myMap.geoObjects
     .add(myPlacemark);
 }
+
+var feedbackLink = document. querySelector(".feedback-link");
+var feedbackPopup = document.querySelector(".feedback-background");
+var feedbackClose = feedbackPopup.querySelector(".feedback-close");
+var feedbackForm = feedbackPopup.querySelector(".feedback-form");
+var feedbackName = feedbackPopup.querySelector(".feedback-name");
+var feedbackEmail = feedbackPopup.querySelector(".feedback-email");
+var feedbackArea = feedbackPopup.querySelector(".feedback-area");
+var feedbackBlock = document.querySelector(".feedback-block");
+
+  feedbackLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.add("feedback-background-show");
+    feedbackName.focus();
+  });
+    feedbackClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    feedbackPopup.classList.remove("feedback-background-show");
+    });
+
+    function onSubmit(evt) {
+      if (!feedbackName.value || !feedbackEmail.value || !feedbackArea.value) {
+      evt.preventDefault();
+      feedbackBlock.classList.add("feedback-block-error");
+      console.log("Введите, пожалуйста, имя, e-mail и сообщение");
+      }
+    }
+
+    feedbackForm.addEventListener("submit", onSubmit);
+    feedbackForm.addEventListener("click", onSubmit);
+    window.addEventListener("keydown", function (evt) {
+      if (evt.keyCode === 27) {
+        if (feedbackPopup.classList.contains("feedback-background-show")) {
+          feedbackPopup.classList.remove("feedback-background-show");
+        }
+      }
+    });
