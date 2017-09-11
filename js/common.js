6df1509848985a23ad5e957979098a833a5c6e24
fox-tracks <t.andriyanova@yahoo.com>
2017-09-11 10:57:35 +0300

    отформатировала js

--- js/common.js
+++ js/common.js
@@ -1,27 +1,28 @@
+// карта
 var myMap;
 var myPlacemark;
-if(window.ymaps){
+if (window.ymaps) {
     ymaps.ready(init);
 }
-function init (){
+function init() {
     myMap = new ymaps.Map('map', {
         center: [59.939536, 30.329119],
         zoom: 16,
         controls: []
     });
-    myPlacemark = new ymaps.Placemark ([59.938631, 30.323055], {
+    myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
         hintContent: 'Мы здесь!'
     }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/pin-and-shadow.png',
-        iconImageSize: [218, 142],
-        iconImageOffset: [-38, -140]
-    });
+            iconLayout: 'default#image',
+            iconImageHref: 'img/pin-and-shadow.png',
+            iconImageSize: [218, 142],
+            iconImageOffset: [-38, -140]
+        });
     myMap.geoObjects
-    .add(myPlacemark);
+        .add(myPlacemark);
 }
-
-var feedbackLink = document. querySelector(".feedback-link");
+// модальное окно и валидация
+var feedbackLink = document.querySelector(".feedback-link");
 var feedbackPopup = document.querySelector(".feedback-background");
 var feedbackClose = feedbackPopup.querySelector(".feedback-close");
 var feedbackForm = feedbackPopup.querySelector(".feedback-form");
@@ -29,45 +30,48 @@ var feedbackName = feedbackPopup.querySelector(".feedback-name");
 var feedbackEmail = feedbackPopup.querySelector(".feedback-email");
 var feedbackArea = feedbackPopup.querySelector(".feedback-area");
 var feedbackBlock = document.querySelector(".feedback-block");
-var textFields = document.querySelectorAll(".text");
-var i;
 
-  feedbackLink.addEventListener("click", function(evt) {
+feedbackLink.addEventListener("click", function (evt) {
     evt.preventDefault();
     feedbackPopup.classList.add("feedback-background-show");
     feedbackName.focus();
-  });
-    feedbackClose.addEventListener("click", function(evt) {
+});
+feedbackClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     feedbackPopup.classList.remove("feedback-background-show");
-    });
+});
 
-    function onSubmit(evt) {
-      if (!feedbackName.value || !feedbackEmail.value || !feedbackArea.value) {
+function onSubmit(evt) {
+    if (!feedbackName.value || !feedbackEmail.value || !feedbackArea.value) {
         evt.preventDefault();
         feedbackBlock.classList.add("feedback-block-error");
-      }
     }
+}
 
-    feedbackForm.addEventListener("submit", onSubmit);
-    feedbackForm.addEventListener("click", onSubmit);
-    window.addEventListener("keydown", function (evt) {
-      if (evt.keyCode === 27) {
+feedbackForm.addEventListener("submit", onSubmit);
+feedbackForm.addEventListener("click", onSubmit);
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
         if (feedbackPopup.classList.contains("feedback-background-show")) {
-          feedbackPopup.classList.remove("feedback-background-show");
+            feedbackPopup.classList.remove("feedback-background-show");
         }
-      }
-    });
-    function onInputChange(evt) {
-      var input = evt.target;
-      if(input.value) {
-          input.classList.remove("text-empty");
-      } else {
-          input.classList.add("text-empty");
-      }
     }
+});
 
-   for (i = 0; i < textFields.length; i++) {
-       textFields[i].addEventListener("change", onInputChange);
-   }
+// признак заполненности текстовых полей
+var textFields = document.querySelectorAll(".text");
+var i;
+
+function onInputChange(evt) {
+    var input = evt.target;
+    if (input.value) {
+        input.classList.remove("text-empty");
+    } else {
+        input.classList.add("text-empty");
+    }
+}
+
+for (i = 0; i < textFields.length; i++) {
+    textFields[i].addEventListener("change", onInputChange);
+}
 
