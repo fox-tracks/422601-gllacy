3bbe158955d3fcdb5ff63e5f64d889693b95a3ac
fox-tracks <t.andriyanova@yahoo.com>
2017-09-12 02:36:54 +0300

    проверила по критериям кроме доступности, убрала лишние картинки, проверила html css, вывели пиксели до features.

--- img/getimg.cmd
+++
@@ -1,3 +0,0 @@
-svgo -f D:\Projects\422601-gllacy\img\svgsprite2 -o D:\Projects\422601-gllacy\img\svgsprite2.min
-
-svg-sprite-generate -d D:\Projects\422601-gllacy\img\svgsprite2.min -o D:\Projects\422601-gllacy\img\sprite.min.svg    
--- js/common.js
+++ js/common.js
@@ -5,16 +5,16 @@ if (window.ymaps) {
     ymaps.ready(init);
 }
 function init() {
-    myMap = new ymaps.Map('map', {
+    myMap = new ymaps.Map("map", {
         center: [59.939536, 30.329119],
         zoom: 16,
         controls: []
     });
     myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
-        hintContent: 'Мы здесь!'
+        hintContent: "Мы здесь!"
     }, {
-            iconLayout: 'default#image',
-            iconImageHref: 'img/pin-and-shadow.png',
+            iconLayout: "default#image",
+            iconImageHref: "img/pin-and-shadow.png",
             iconImageSize: [218, 142],
             iconImageOffset: [-38, -140]
         });
